9485388928ae39de0de6e13fa47bfcdf2e81979a
Daniel <115094794+dvan-kle@users.noreply.github.com>
2023-04-21 02:58:57 +0200

    21/04

---
+++ src/reverse_rotate.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   instructions.c                                     :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: dvan-kle <dvan-kle@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2023/01/24 15:31:50 by dvan-kle      #+#    #+#                 */
+/*   Updated: 2023/04/21 02:56:40 by dvan-kle      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../incl/push_swap.h"
+
+// Shift down all elements of stack a by 1.
+void rra()
+{
+    
+}
+
+// Shift down all elements of stack b by 1.
+void rrb()
+{
+    
+}
+
+// rra and rrb at the same time.
+void rrr()
+{
+    
+}
