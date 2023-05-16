a3328e03d7bb5a9864a55e99966bae16ab60a5c8
Daniel <115094794+dvan-kle@users.noreply.github.com>
2023-05-17 01:22:18 +0200

    17/05

--- incl/push_swap.h
+++ incl/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: dvan-kle <dvan-kle@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/01/20 21:03:49 by dvan-kle      #+#    #+#                 */
-/*   Updated: 2023/04/21 00:44:28 by dvan-kle      ########   odam.nl         */
+/*   Updated: 2023/05/04 16:34:23 by dvan-kle      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,17 +24,33 @@ typedef struct s_list
 }	t_list;
 
 void	error_return(char *str);
-void    check_input(int argc, char **argv);
-
+void	check_input(int argc, char **argv);
+//List
 void	ft_lstadd_back(t_list **stack, t_list *new);
 t_list	*ft_lstlast(t_list *stack);
 t_list	*ft_lstnew(int content);
 int		ft_lstsize(t_list *stack);
 void	printlist(t_list *stack);
-
-int	swap(t_list **stack);
-int	sa(t_list **stack_a);
-int	sb(t_list **stack_b);
-int	ss(t_list **stack_a, t_list **stack_b);
+//Instructions
+int		swap(t_list **stack);
+int		sa(t_list **stack_a);
+int		sb(t_list **stack_b);
+int		ss(t_list **stack_a, t_list **stack_b);
+int		push(t_list **from_stack, t_list **to_stack);
+void	pa(t_list **stack_a, t_list **stack_b);
+void	pb(t_list **stack_b, t_list **stack_a);
+void	rotate(t_list **stack);
+void	ra(t_list **stack_a);
+void	rb(t_list **stack_b);
+void	rr(t_list **stack_a, t_list **stack_b);
+int		reverserotate(t_list **stack);
+void	rra(t_list **stack_a);
+void	rrb(t_list **stack_b);
+void	rrr(t_list **stack_a, t_list **stack_b);
+//Sorting
+int		sorted_check(t_list *stack);
+void	sort2(t_list **stack_a);
+void	sort3(t_list **stack_a, t_list **stack_b);
+void	simple_sort(t_list **stack_a, t_list **stack_b);
 
 #endif
